582c742455a1204d9714f876f0e6a1c59a647a02
Cazeq <emailenplus2@gmail.com>
2022-01-26 17:54:55 +0100

    init

---
+++ repository/user.repository.js
@@ -0,0 +1,13 @@
+/**
+ * User.repo.js
+ * findAllUsers - 
+ * FindUserById - id
+ * DeleteUserById -id 
+ * updateUserById -id & user
+ * insertUser
+ * 
+ * Role.repo.js
+ * findAllRolesByUserId
+ * updateRoleByUserId
+ * 
+ */
